60ee26a9161e6e740f67ef4fd5071fe54e6c4b14
User <josselynuni@gmail.com>
2025-10-05 15:41:41 -0500

    Tarea02_Reyes_Josselyn

--- src/ejercicio04.java
+++ src/ejercicio04.java
@@ -15,9 +15,8 @@ public class ejercicio04 {
         System.out.println("Altura: ");
         double altura= estudiante.nextDouble();
 
-        if (edad < 18 && altura < 1.60){
+        if (edad < 18 || altura < 1.60){
             System.out.println(nombre +" no cumple con los requisitos.");
-
         }else{
             System.out.println(nombre + " fue aceptado para el equipo");
 
